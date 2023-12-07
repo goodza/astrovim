de716b8c25e07690697db36de5808c55b00dbe97
goodza <henryclash@gmail.com>
2023-12-08 02:20:09 +0300

    add catpuchin theme

---
+++ plugins/scheme.lua
@@ -0,0 +1,11 @@
+return { 
+  "catppuccin/nvim", 
+  name = "catppuccin",
+  flavour = "mocha", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+        light = ":latte",
+        dark = "mocha",
+    },
+    transparent_background = true,
+}
+
